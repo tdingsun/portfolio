b7acebaf3f3c57049fd6ced6ba9ac029861c6a97
Tiger <tiger.dingsun@gmail.com>
2020-02-03 11:35:05 -0500

    added posters

--- src/app/project-list.ts
+++ src/app/project-list.ts
@@ -6,7 +6,8 @@ export const PROJECTS: Project[] = [
         season: 'Ongoing',
         imgRoot: 'misc-posters',
         imgs: [
-            '1.jpg', 'd.jpg', 'a.jpg', 'b.jpg', '2.jpg', '4.jpg',
+            '1.jpg', '2.png', 'd.jpg', 'g.jpg', 'h.jpg', 'i.jpg', 'f.jpg',
+            'a.jpg', 'b.jpg', '4.jpg',
             '3.jpg', 'c.jpg', '5.jpg',
             '6.jpg', '7.jpg', '8.jpg', '9.jpg',
             '10.jpg', '11.jpg', '12.jpg', '13.jpg',
